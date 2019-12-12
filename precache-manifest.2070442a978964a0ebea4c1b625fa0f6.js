26447b15ed1d99e62adb251e4e3ed847fdb59ac3
Shiva Reddy <shiva.animireddy@qvantel.com>
2019-12-13 00:45:34 +0530

    Updates

---
+++ precache-manifest.2070442a978964a0ebea4c1b625fa0f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18da7a234a4dda47452329d302c308a1",
+    "url": "/shoppinglist/index.html"
+  },
+  {
+    "revision": "ca15a2cb406a99112c33",
+    "url": "/shoppinglist/static/css/main.8b5427b2.chunk.css"
+  },
+  {
+    "revision": "a174f9446d94e597d928",
+    "url": "/shoppinglist/static/js/2.a5d7af4a.chunk.js"
+  },
+  {
+    "revision": "ca15a2cb406a99112c33",
+    "url": "/shoppinglist/static/js/main.439618fc.chunk.js"
+  },
+  {
+    "revision": "5ecd3ebea86738ca462b",
+    "url": "/shoppinglist/static/js/runtime~main.ad60753e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppinglist/static/media/logo.5d5d9eef.svg"
+  }
+]);
